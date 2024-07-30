2c0fe0dfea9cde86006c7c090f3e7a9b24857276
gustavormuller <gusrmuller@gmail.com>
2024-07-29 23:59:17 -0300

    feat: initial heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
     <div className="bg-slate-50">
       <section>
-        <MaxWidthWrapper>
-          
+        <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                <img src="/snake-1.png" className="w-full" alt="Logo"></img>
+              </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl mf:text-6xl lg:text-7xl">
+                Sua imagem numa capinha de celular <span>customizada</span>
+              </h1>
+            </div>
+          </div>
         </MaxWidthWrapper>
       </section>
     </div>
