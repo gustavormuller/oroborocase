d516e0e83dd5437b6ccc6206d7074db83d2b6cfa
gustavormuller <gusrmuller@gmail.com>
2024-08-01 06:51:06 -0300

    feat: lucide react icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Check } from "lucide-react";
 
 export default function Home() {
   return (
@@ -12,8 +13,33 @@ export default function Home() {
                 <img src="/snake-1.png" className="w-full" alt="Logo"></img>
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl mf:text-6xl lg:text-7xl">
-                Sua imagem numa capinha de celular <span>customizada</span>
+                Sua imagem numa capinha de celular{" "}
+                <span className="bg-green-600 px-2 text-white">
+                  customizada
+                </span>
               </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Capture suas memórias favoritas com sua capinha
+                <span className="font-semibold"> exclusiva</span>. OroboroCase
+                protege suas memórias e não apenas seu celular.
+              </p>
+
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    Material de alta qualidade
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    5 anos de garantia de impressão
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    Suporte aos modelos mais modernos de iPhone
+                  </li>
+                </div>
+              </ul>
             </div>
           </div>
         </MaxWidthWrapper>
