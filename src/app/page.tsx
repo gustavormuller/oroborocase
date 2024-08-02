fd115a7ec0198997af0b4d114c693f6d14631732
gustavormuller <gusrmuller@gmail.com>
2024-08-02 08:40:14 -0300

    feat: testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Check } from "lucide-react";
+import { Check, Star } from "lucide-react";
 
 export default function Home() {
   return (
@@ -23,7 +23,6 @@ export default function Home() {
                 <span className="font-semibold"> exclusiva</span>. OroboroCase
                 protege suas memórias e não apenas seu celular.
               </p>
-
               <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
                 <div className="space-y-2">
                   <li className="flex gap-1.5 items-center text-left">
@@ -31,8 +30,8 @@ export default function Home() {
                     Material de alta qualidade
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 text-green-600" />
-                    5 anos de garantia de impressão
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />5 anos
+                    de garantia de impressão
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
                     <Check className="h-5 w-5 shrink-0 text-green-600" />
@@ -40,6 +39,36 @@ export default function Home() {
                   </li>
                 </div>
               </ul>
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
+                <div className="flex -space-x-4">
+                  {Array(5)
+                    .fill(null)
+                    .map((_, index) => (
+                      <img
+                        key={index}
+                        src={`/users/user-${index + 1}.png`}
+                        className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100 object-cover"
+                        alt="Imagem de usuário"
+                      />
+                    ))}
+                </div>
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    {Array(5)
+                      .fill(null)
+                      .map((_, index) => (
+                        <Star
+                          key={index}
+                          className="h-4 w-4 text-green-600 fill-green-600"
+                        />
+                      ))}
+                  </div>
+                  <p>
+                    <span className="font-semibold">1.187</span> clientes
+                    satisfeitos
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
