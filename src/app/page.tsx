6052ed8149f1459234f8fe23fe70b409cbe50f16
gustavormuller <gusrmuller@gmail.com>
2024-08-12 14:56:07 -0300

    feat: h2 image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,10 +98,16 @@ export default function Home() {
             <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
               O que nossos{" "}
               <span className="relative px-2">
-                clientes <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+                clientes{" "}
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
               </span>{" "}
               dizem
             </h2>
+            <img
+              src="/snake-2.png"
+              className="w-24 order-0 lg:order-2"
+              alt="oroboro"
+            />
           </div>
         </MaxWidthWrapper>
       </section>
