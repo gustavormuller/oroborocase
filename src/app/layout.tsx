b78efd2c0aaa9812249d966e22b3671cd3ee058a
gustavormuller <gusrmuller@gmail.com>
2024-08-20 14:40:04 -0300

    feat: upload page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={inter.className}>
         <Navbar />
-        {children}
-        <Footer />
+        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">{children}</div>
+          <Footer />
+        </main>
       </body>
     </html>
   );
