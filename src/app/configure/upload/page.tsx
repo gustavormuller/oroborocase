aa77bcf5fe4084276854c8ba04d736e79520cd8b
gustavormuller <gusrmuller@gmail.com>
2024-08-21 16:44:11 -0300

    feat: upload progress bar

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,16 +1,23 @@
+/* eslint-disable jsx-a11y/alt-text */
 "use client";
 
+import { Progress } from "@/components/ui/progress";
 import { cn } from "@/lib/utils";
-import { useState } from "react";
+import { Image, Loader2, MousePointerSquareDashed } from "lucide-react";
+import { useState, useTransition } from "react";
 import Dropzone, { FileRejection } from "react-dropzone";
 
 const Page = () => {
   const [isDragOver, setIsDragOver] = useState<boolean>(false);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
 
   const onDropRejected = () => {};
   const onDropAccepted = () => {
     console.log("accepted");
   };
+
+  const isUploading = false;
+  const [isPending, startTransition] = useTransition();
 
   return (
     <div
@@ -39,7 +46,27 @@ const Page = () => {
               {...getRootProps()}
             >
               <input {...getInputProps()} />
-              
+              {isDragOver ? (
+                <MousePointerSquareDashed className="h-6 w-6 text-zinc-500 mb-2 " />
+              ) : isUploading || isPending ? (
+                <Loader2 className="animate-spin h-6 w-6 text-zinc-500 mb-2" />
+              ) : (
+                <Image className="h-6 w-6 text-zinc-500 mb-2" />
+              )}
+              <div className="flex flex-col justify-center mb-2 text-sm text-zinc-700">
+                {isUploading ? (
+                  <div className="flex flex-col items-center">
+                    <p>Carregando...</p>
+                    <Progress value={uploadProgress} className="mt-2 w-40 h-2 bg-gray-300" />
+                  </div>
+                ) : isPending ? (
+                  <div></div>
+                ) : isDragOver ? (
+                  <span></span>
+                ) : (
+                  <span></span>
+                )}
+              </div>
             </div>
           )}
         </Dropzone>
